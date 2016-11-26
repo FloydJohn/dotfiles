a6938571bff12e240975c675a22378d0417b9d0f
FloydJohn <FloydJohn@users.noreply.github.com>
2016-11-26 12:35:40 +0100

    Removed atom. Added polybar config

--- atom/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#This script should install Atom and all my starred plugins.
-echo "Installing atom-editor-bin (and gnome-keyring if needed)..."
-pacaur -S --needed --noedit --noconfirm -q atom-editor-bin gnome-keyring
-
-apm install --starred
